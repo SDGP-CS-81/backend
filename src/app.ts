cfb20cca95823e85a6379622667c653a45cea090
AfeefRazick <afeefrazickamir@gmail.com>
2024-01-19 23:34:53 +0530

    refactor: server connections from app.ts to index.ts

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,8 @@
 import express, { Request, Response, Application } from "express";
-import dotenv from "dotenv";
-dotenv.config();
-import mongoose from "mongoose";
 import cors from "cors";
 import { vidInfoRouter } from "./routes/vidInfo";
-const app: Application = express();
+
+export const app: Application = express();
 
 // middleware
 app.use(express.json());
@@ -16,16 +14,3 @@ app.use("/api", vidInfoRouter);
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello world");
 });
-
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
-});
-
-mongoose
-  .connect(process.env.MONGO_URI!)
-  .then(() => {
-    console.log("Connected to MongoDB Successfully");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
