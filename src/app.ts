a45e207ca8679fd1ada4f77920d6cb842d81d140
Ravisara <ravisarahdodampegamage@gmail.com>
2024-01-28 02:23:24 +0530

    integrate resnet controller into  app.ts and correct model.json file path

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,28 @@
+// import express, { Request, Response, Application } from "express";
+// import cors from "cors";
+// import { vidInfoRouter } from "./routes/vidInfo";
+
+// export const app: Application = express();
+
+// // middleware
+// app.use(express.json());
+// app.use(cors());
+
+// // routes
+// app.use("/api", vidInfoRouter);
+
+// app.get("/", (req: Request, res: Response) => {
+//   res.send("Hello world");
+// });
+
 import express, { Request, Response, Application } from "express";
 import cors from "cors";
 import { vidInfoRouter } from "./routes/vidInfo";
+import ResnetController from "./controllers/ResnetController";
 
 export const app: Application = express();
+
+const resnetcontroller = ResnetController()
 
 // middleware
 app.use(express.json());
@@ -11,6 +31,12 @@ app.use(cors());
 // routes
 app.use("/api", vidInfoRouter);
 
+app.use("/model", express.static("model"));
+
+app.get("/predict", resnetcontroller.predict)
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello world");
 });
+
+//http://localhost:5000/predict
