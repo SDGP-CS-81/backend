60888562e1fc94c8cdf9e62de6c72b8ef8e48b5e
AbrarSL <abrarsl2002@gmail.com>
2024-02-21 01:08:35 +0530

    feat: Hook everything up
    This commit actually contains many things. The most obvious change is
    that all the other code is actually used to generate a response! Other
    than that there was also a need to set up CORS. This commit also
    implements the route in such a way that if YT categorization is
    available the frames are never downloaded and classified leading to a
    massive speed boost
    
    Closes: #28

--- video-analysis-service/app/main.py
+++ video-analysis-service/app/main.py
@@ -1,27 +1,65 @@
-from fastapi import FastAPI, Query
-from pydantic import BaseModel
-from typing import List
-from .controllers import classify
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import Json
+from app.video_analyser import VideoAnalyser
+from app.video_downloader import VideoDownloader
+from app.image_classifier import ImageClassifier
 
 app = FastAPI()
+# Restrict this when we deploy
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
-# default route
 @app.get("/")
 def root_route():
     return {"error": "Use GET /video-analysis instead"}
 
 
 @app.get("/video-analysis")
-async def video_analysis_route(video_id: str):
-    # category_scores = await classify()# pass in image
-    category_scores = "dummy data"
-    frame_scores = {"detailScore": 444}
-    print("video_id:", video_id)
-    return {"categoryScores": category_scores, "frameScores": frame_scores}
+async def video_analysis_route(video_id: str, category_keywords: Json):
+    vid_dl = VideoDownloader(video_id)
+    video_category, video_text_data = await vid_dl.get_video_text_info()
 
+    keyword_scores = VideoAnalyser.calculate_text_scores(
+        video_text_data, category_keywords
+    )
 
-# pip install -r requirements.txt
-# uvicorn src.main:app --reload
-# then in a new terminal
-# curl -X POST "http://127.0.0.1:8000/prediction/" -H  "accept: application/json" -H  "Content-Type: multipart/form-data" -F "file=@image.jpg"
+    response_data = {
+        "video_id": video_id,
+        "category_scores": {key: 0 for key in ImageClassifier.CLASS_NAMES},
+        "frame_scores": {"detail_score": 0, "diff_score": 0},
+        "keyword_scores": keyword_scores,
+    }
+
+    # Hack to use YT category to boost score and exit early
+    # This should be synchronized with the frontend
+    if "music" in video_category[0].lower():
+        keyword_scores["music"] = 1000
+        return response_data
+    elif "gaming" in video_category[0].lower():
+        keyword_scores["gaming"] = 1000
+        return response_data
+
+    video_frames = await vid_dl.get_video_frames()
+    vid_analyser = VideoAnalyser(video_frames)
+    (
+        detail_score,
+        diff_score,
+        selected_frame,
+    ) = await vid_analyser.calculate_frame_scores()
+
+    category_scores = await ImageClassifier.classify_frame(selected_frame)
+
+    response_data["category_scores"] = category_scores
+    response_data["frame_scores"] = {
+        "detail_score": detail_score,
+        "diff_score": diff_score,
+    }
+
+    return response_data
