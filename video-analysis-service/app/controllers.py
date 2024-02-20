4bd5661d3cf011c111164ef284d293dadea4d146
AbrarSL <abrarsl2002@gmail.com>
2024-02-20 22:17:56 +0530

    style: Make style and indentation consistent

--- video-analysis-service/app/controllers.py
+++ video-analysis-service/app/controllers.py
@@ -1,35 +1,37 @@
-import numpy as np;
-from PIL import Image;
-import sys;
+import numpy as np
+from PIL import Image
+import sys
 from fastapi import HTTPException
 from .constants import classNames
 from tensorflow.keras.models import load_model
 
-filepath='./model/model.h5'
-model=load_model(filepath,compile=True)
+filepath = "./model/model.h5"
+model = load_model(filepath, compile=True)
+
 
 async def classify():
-  # file = 'image.jpg'
-  # if file.content_type.startswith('image/') is False:
-  #   raise HTTPException(status_code=400, detail=f'File \'{file.filename}\' is not an image.')
+    # file = 'image.jpg'
+    # if file.content_type.startswith('image/') is False:
+    #   raise HTTPException(status_code=400, detail=f'File \'{file.filename}\' is not an image.')
 
-  try:
-    # rewrite this step
-    # contents = await file.read()
-    # pil_image = Image.open(io.BytesIO(contents))
+    try:
+        # rewrite this step
+        # contents = await file.read()
+        # pil_image = Image.open(io.BytesIO(contents))
 
-    pil_image = pil_image.resize((224, 224))
+        pil_image = pil_image.resize((224, 224))
 
-    numpy_image = np.array(pil_image).reshape((224, 224, 3))
+        numpy_image = np.array(pil_image).reshape((224, 224, 3))
 
-    prediction_array = np.array([numpy_image])
-    prediction_batch = model.predict(prediction_array)
-    prediction = prediction_batch[0]
-    # predicted_class = np.argmax(prediction)
-    category_scores = {class_name: score for class_name, score in zip(classNames, prediction) }
-    return category_scores
-  
-  except:
-    e = sys.exc_info()[1]
-    raise HTTPException(status_code=500, detail=str(e))
-   
+        prediction_array = np.array([numpy_image])
+        prediction_batch = model.predict(prediction_array)
+        prediction = prediction_batch[0]
+        # predicted_class = np.argmax(prediction)
+        category_scores = {
+            class_name: score for class_name, score in zip(classNames, prediction)
+        }
+        return category_scores
+
+    except:
+        e = sys.exc_info()[1]
+        raise HTTPException(status_code=500, detail=str(e))
--- video-analysis-service/app/main.py
+++ video-analysis-service/app/main.py
@@ -1,28 +1,27 @@
-from fastapi import FastAPI,Query
-from pydantic import BaseModel;
-from typing import List;
-from .controllers import classify;
+from fastapi import FastAPI, Query
+from pydantic import BaseModel
+from typing import List
+from .controllers import classify
 
 app = FastAPI()
 
 
-#default route
-@app.get('/')
+# default route
+@app.get("/")
 def root_route():
-  return { 'error': 'Use GET /video-analysis instead' }
+    return {"error": "Use GET /video-analysis instead"}
 
-@app.get('/video-analysis')
+
+@app.get("/video-analysis")
 async def video_analysis_route(video_id: str):
-  # category_scores = await classify()# pass in image
-  category_scores = "dummy data"
-  frame_scores = {'detailScore':444}
-  print('video_id:',video_id)
-  return {'categoryScores': category_scores, 'frameScores': frame_scores}
+    # category_scores = await classify()# pass in image
+    category_scores = "dummy data"
+    frame_scores = {"detailScore": 444}
+    print("video_id:", video_id)
+    return {"categoryScores": category_scores, "frameScores": frame_scores}
 
 
-
-
-#pip install -r requirements.txt
+# pip install -r requirements.txt
 # uvicorn src.main:app --reload
-#then in a new terminal
-#curl -X POST "http://127.0.0.1:8000/prediction/" -H  "accept: application/json" -H  "Content-Type: multipart/form-data" -F "file=@image.jpg"
+# then in a new terminal
+# curl -X POST "http://127.0.0.1:8000/prediction/" -H  "accept: application/json" -H  "Content-Type: multipart/form-data" -F "file=@image.jpg"
