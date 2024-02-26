8cac46f6ec4b4792b5498cd067ac419aab953495
AbrarSL <abrarsl2002@gmail.com>
2024-02-26 10:30:26 +0530

    test: Cleanup downloader tests and add docs

--- video-analysis-service/tests/test_video_downloader.py
+++ video-analysis-service/tests/test_video_downloader.py
@@ -8,43 +8,60 @@ class VideoDownloaderTest(IsolatedAsyncioTestCase):
         self.video_ids = ["3qLs27KTYAg", "IOzkOXSz9gE"]
 
     async def test_text_data_no_exception(self):
-        [
-            self.assertTrue(not (isinstance(result, VideoDownloaderError)))
-            for result in await asyncio.gather(
-                *(VideoDownloader(id).get_video_text_info() for id in self.video_ids),
-                return_exceptions=True,
+        """
+        Test to ensure that no exceptions are thrown with expected usage
+        """
+        for result in await asyncio.gather(
+            *(VideoDownloader(id).get_video_text_info() for id in self.video_ids),
+            return_exceptions=True,
+        ):
+            self.assertTrue(
+                not (isinstance(result, VideoDownloaderError)),
+                "Expected text data!",
             )
-        ]
 
     async def test_frame_data_no_exception(self):
-        [
-            self.assertTrue(not (isinstance(result, VideoDownloaderError)))
-            for result in await asyncio.gather(
-                *(VideoDownloader(id).get_video_frames() for id in self.video_ids),
-                return_exceptions=True,
+        """
+        Test to ensure that no exceptions are thrown with expected usage
+        """
+        for result in await asyncio.gather(
+            *(VideoDownloader(id).get_video_frames() for id in self.video_ids),
+            return_exceptions=True,
+        ):
+            self.assertTrue(
+                not (isinstance(result, VideoDownloaderError)), "Expected image data!"
             )
-        ]
 
     async def test_text_data_category_not_empty(self):
-        [
-            self.assertLess(0, len(result[0]))
-            for result in await asyncio.gather(
-                *(VideoDownloader(id).get_video_text_info() for id in self.video_ids),
-            )
-        ]
+        """
+        Test to ensure that the video category is properly populated
+        """
+        for result in await asyncio.gather(
+            *(VideoDownloader(id).get_video_text_info() for id in self.video_ids)
+        ):
+            self.assertLess(0, len(result[0]), "Expected more than 0 categories!")
 
     async def test_text_data_text_not_empty(self):
-        [
-            self.assertLess(0, len(result[1]))
-            for result in await asyncio.gather(
-                *(VideoDownloader(id).get_video_text_info() for id in self.video_ids),
+        """
+        Test to ensure that text data is properly populated
+        """
+        for result in await asyncio.gather(
+            *(VideoDownloader(id).get_video_text_info() for id in self.video_ids)
+        ):
+            self.assertLess(
+                0,
+                len(result[1]),
+                # Assuming that at least video titles are not empty
+                "Expected more than 0 characters of text!",
             )
-        ]
 
     async def test_frame_data_not_empty(self):
-        [
-            self.assertLess(0, len(result))
-            for result in await asyncio.gather(
-                *(VideoDownloader(id).get_video_frames() for id in self.video_ids),
-            )
-        ]
+        """
+        Test to ensure that downloader returns at least 2 images
+
+        We check for 2 since diffing requires at least 2 images
+        """
+        for result in await asyncio.gather(
+            *(VideoDownloader(id).get_video_frames() for id in self.video_ids)
+        ):
+            self.assertLess(1, len(result), "Expected at least 2 images!")
