1ee6b1838450ecf3b335999bc99513bcd4885091
Ravisara Hasaranga <ravisarahdodampegamage@gmail.com>
2024-03-12 23:35:20 +0530

    feat: implement express rate limiter for api endpoints

--- cache-service/src/app.ts
+++ cache-service/src/app.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response, Application } from "express";
 import cors from "cors";
+import rateLimit from "express-rate-limit";
 
 import { videoRouter } from "./routes/video";
 import { channelRouter } from "./routes/channel";
@@ -10,9 +11,15 @@ export const app: Application = express();
 app.use(express.json());
 app.use(cors());
 
-// routes
-app.use("/api", videoRouter);
-app.use("/api", channelRouter);
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // 5 minute
+  max: 300, //maximum 300 requests per 5 minutes
+  message: "Too many requests, please try again later.",
+});
+
+// Apply the rate limiter to the routes
+app.use("/api", limiter, videoRouter);
+app.use("/api", limiter, channelRouter);
 
 app.get("/", (req: Request, res: Response) => {
   res.status(404).send("Prefix API routes with '/api'");
